8eb41ae6035a5075d630ef5315e813966a8bf1a4
fazliddin <fazliddin>
2024-03-31 23:02:02 +0300

    js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let str = "Lorem ipsum, dolor sit, amet consectetur, adipisicing elit. fugit, accusamus?"
+let str = prompt('iltimos matn kriting')
+while(str==0){
+  let str = prompt('xatolik yuz berdi iltimos boshqattan yozing')
+}
 let arr = str.split('')
 for (let i = 0; i < arr.length; i++) {
     if (arr[i] == 'o' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'a' || arr[i] == 'u') {
